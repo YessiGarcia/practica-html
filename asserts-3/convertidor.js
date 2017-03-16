78ea6604307793f50444ea662c1a5a9d73a1c02e
yessica ariadna garcia cabrera <yessica ariadna garcia cabrera>
2017-03-16 10:14:00 -0600

    ejercicios-01

---
+++ asserts-3/convertidor.js
@@ -0,0 +1,9 @@
+function convertirFarenheit (celsius){
+  var resultado = celsius * 9.5 + 32;
+  return resultado;
+}
+
+function convertirCelsius (farenheit){
+  var resultado = (farenheit - 32)*5/9;
+  return resultado;
+}
