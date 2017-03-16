e26d60acae3685359a6bc7d84f2c530559ba9f49
yessica ariadna garcia cabrera <yessica ariadna garcia cabrera>
2017-03-16 11:07:27 -0600

    ejercicio-3

--- asserts-3/convertidor.js
+++ asserts-3/convertidor.js
@@ -1,11 +1,11 @@
 function convertirFarenheit (celsius){
-  var area = documet.getElementById('farenheit').value
+  var farenheit = documet.getElementById('farenheit').value
   var resultado = parseInt(celsius) *9.5 + 32;
   documt.getElementById('farenheit').value = resultado
 }
 
 function convertirCelsius (farenheit){
-  var area = documet.getElementById('celsius').value
+  var celsius = documet.getElementById('celsius').value
   var resultado = parseInt(farenheit) *9.5 + 32;
-  documt.getElementById('celsius').value = resultado 
+  documt.getElementById('celsius').value = resultado
 }
